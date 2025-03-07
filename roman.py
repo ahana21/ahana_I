f521778d060c35a060ef68af2a812ada46ac3c97
ahana21 <koua280@aa.edu>
2025-03-07 15:11:17 -0700

    Test 3 : PASS

--- roman.py
+++ roman.py
@@ -1,2 +1,20 @@
 def convert_roman_numerals(s):
-     return 2 if s == "II" else 1
+    if s == "I":
+        return 1
+    return 0
+
+def convert_roman_numerals(s):
+    if s == "II":
+        return 2
+    if s == "I":
+        return 1
+    return 0
+
+def convert_roman_numerals(s):
+    if s == "III":
+        return 3
+    if s == "II":
+        return 2
+    if s == "I":
+        return 1
+    return 0
--- test_roman.py
+++ test_roman.py
@@ -1,13 +1,16 @@
 import unittest     
-from roman import convert_roman_numerals
+from roman import convert_roman_numerals  
 
-class TestRoman(unittest.TestCase):
-    def test_I(self):
-        self.assertEqual(convert_roman_numerals("I"),1)
+class TestRoman(unittest.TestCase):  
+    def test_I(self):  
+        self.assertEqual(convert_roman_numerals("I"), 1)  
 
-    def test_II(self):
-         self.assertEqual(convert_roman_numerals("II"),2)
+    def test_II(self):  
+        self.assertEqual(convert_roman_numerals("II"), 2) 
 
+    def test_III(self):  
+        self.assertEqual(convert_roman_numerals("III"), 3)  
+ 
 
-if __name__=="__main__":
+if __name__ == "__main__":  
     unittest.main()
